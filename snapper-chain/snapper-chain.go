cff77f31004269b0d5f12069af995973d8ca4065
Matt Bonsall <matt.bonsall.v@gmail.com>
2018-03-30 00:56:24 -0500

    solved small/large snapper-chain

--- snapper-chain/snapper-chain.go
+++ snapper-chain/snapper-chain.go
@@ -1,5 +1,35 @@
+/*
+	Code Jam 2010 Qualification Snapper Chain
+	This solution was derived by descovering that the bitset of
+	power state was being iterated by 1, meaning that if the smallest
+	n digits of k were all equal to 1, the power at n would be ON.
+*/
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var ncases int
+	fmt.Scan(&ncases)
+	for casenum := 1; casenum <= ncases; casenum++ {
+		var n, k int
+		fmt.Scan(&n, &k)
+		var o string
+		if snap(n, k) {
+			o = "ON"
+		} else {
+			o = "OFF"
+		}
+		fmt.Printf("Case #%d: %s\n", casenum, o)
+	}
+
+}
+
+func snap(n, k int) bool {
+	bitmask := int(math.Exp2(float64(n))) - 1
+	return k&bitmask == bitmask
 }
